b7730fbf382c5a884310673701af8cea15ed73a4
Ansh Kataria <ansh.kataria@student.uq.edu.au>
2026-06-30 09:55:25 +1000

    add paged response for paginated results

---
+++ backend/src/main/java/com/pitstop/backend/dto/PagedResponse.java
@@ -0,0 +1,19 @@
+package com.pitstop.backend.dto;
+
+import lombok.Builder;
+import lombok.Data;
+import lombok.Value;
+
+import java.util.List;
+
+@Data
+@Builder
+public class PagedResponse<T> {
+
+    List<T> content;
+    int page;
+    int size;
+    long totalElements;
+    int totalPages;
+    boolean last;
+}
